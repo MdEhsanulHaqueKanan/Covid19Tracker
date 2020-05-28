1553f8275b2a6fcb022f7a553fb9d37deb01cb78
Kanan <csc103.fall2010.10305038@gmail.com>
2020-05-29 03:21:20 +0600

    Fixed issue on index.js file inside api folder

--- src/api/index.js
+++ src/api/index.js
@@ -5,43 +5,35 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async (country) => {
     let changeableUrl = url;
 
-    if(country){
+    if (country) {
         changeableUrl = `${url}/countries/${country}`;
     }
 
     try {
-        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableUrl);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
 
-        return {confirmed, recovered, deaths, lastUpdate};
+        return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
-        console.log(error);
+        return error;
     }
-}
+};
 
-export const fetchDailyData = async ()=> {
+export const fetchDailyData = async () => {
     try {
         const { data } = await axios.get(`${url}/daily`);
 
-        const modifiedData = data.map((dailyData) => ({
-            confirmed: dailyData.confirmed.total,
-            deaths: dailyData.deaths.total,
-            data: dailyData.reportDate,
-        }));
-
-        return modifiedData;
-        
-        } catch (error) {
-            console.log(error)
-        }
-}
+        return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+    } catch (error) {
+        return error;
+    }
+};
 
 export const fetchCountries = async () => {
     try {
-        const { data: { countries }} = await axios.get(`${url}/countries`);
+        const { data: { countries } } = await axios.get(`${url}/countries`);
 
-        return countries.map((country) => country.name)
+        return countries.map((country) => country.name);
+    } catch (error) {
+        return error;
     }
-    catch (error) {
-        console.log(error);
-    }
-}
+};
